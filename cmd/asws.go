9c1a5d480747369806f822a73e1710094445c357
Craig Johnston <cj@imti.co>
2020-12-09 12:51:06 -0800

    update build process, code cleanup, flag support.

--- cmd/asws.go
+++ cmd/asws.go
@@ -1,63 +1,119 @@
 package main
 
 import (
-	"log"
+	"flag"
 	"net/http"
 	"os"
+	"runtime"
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 
-	"github.com/gin-contrib/zap"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 )
 
+var (
+	ipEnv          = getEnv("IP", "127.0.0.1")
+	portEnv        = getEnv("PORT", "80")
+	staticDirEnv   = getEnv("STATIC_DIR", "./www")
+	staticPathEnv  = getEnv("STATIC_PATH", "/")
+	fsEnabledEnv   = getEnv("FS_ENABLED", "no")
+	fsDirEnv       = getEnv("FS_DIR", "./files")
+	fsPathEnv      = getEnv("FS_PATH", "/files")
+	debugEnv       = getEnv("DEBUG", "false")
+	metricsEnv     = getEnv("METRICS", "true")
+	metricsPortEnv = getEnv("METRICS_PORT", "2112")
+)
+
+var Version = "0.0.0"
+var Service = "asws"
+
 func main() {
 
-	port := getEnv("PORT", "80")
-	staticDir := getEnv("STATIC_DIR", "./www")
-	staticPath := getEnv("STATIC_PATH", "/")
-	fsEnabled := getEnv("FS_ENABLED", "no")
-	fsDir := getEnv("FS_DIR", "./files")
-	fsPath := getEnv("FS_PATH", "/files")
-	debug := getEnv("DEBUG", "false")
-	metrics := getEnv("METRICS", "true")
-	metricsPort := getEnv("METRICS_PORT", "9696")
+	var (
+		ip          = flag.String("ip", ipEnv, "bind ip")
+		port        = flag.String("port", portEnv, "port")
+		staticDir   = flag.String("staticDir", staticDirEnv, "static dir")
+		staticPath  = flag.String("staticPath", staticPathEnv, "static path")
+		fsEnabled   = flag.String("fsEnabled", fsEnabledEnv, "filesystem enabled")
+		fsDir       = flag.String("fsDir", fsDirEnv, "filesystem directory")
+		fsPath      = flag.String("fsPath", fsPathEnv, "filesystem path")
+		debug       = flag.String("debug", debugEnv, "debug")
+		metrics     = flag.String("metrics", metricsEnv, "metrics")
+		metricsPort = flag.String("metricsPort", metricsPortEnv, "metrics port")
+	)
+	flag.Parse()
+
+	// add some useful info to metrics
+	promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: "www",
+		Name:      "info",
+		ConstLabels: prometheus.Labels{
+			"go_version": runtime.Version(),
+			"version":    Version,
+			"service":    Service,
+		},
+	}).Inc()
 
 	gin.SetMode(gin.ReleaseMode)
 
-	if debug == "true" {
+	if *debug == "true" {
 		gin.SetMode(gin.DebugMode)
 	}
 
 	r := gin.New()
 
-	logger, _ := zap.NewProduction()
+	zapCfg := zap.NewProductionConfig()
+	zapCfg.DisableCaller = true
+	zapCfg.DisableStacktrace = true
 
-	if debug == "true" {
+	logger, _ := zapCfg.Build()
+
+	if *debug == "true" {
 		logger, _ = zap.NewDevelopment()
 	}
 
 	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
 
-	if fsEnabled == "yes" {
-		r.StaticFS(fsPath, http.Dir(fsDir))
+	if *fsEnabled == "yes" {
+		r.StaticFS(*fsPath, http.Dir(*fsDir))
 	}
 
-	r.Static(staticPath, staticDir)
+	r.Static(*staticPath, *staticDir)
 
-	// Prometheus Metrics
-	if metrics == "true" {
+	// metrics server (run in go routine)
+	if *metrics == "true" {
 		go func() {
 			http.Handle("/metrics", promhttp.Handler())
-			log.Fatal(http.ListenAndServe(":"+metricsPort, nil))
-			r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+			logger.Info("Starting ASWS Metrics Server",
+				zap.String("type", "start_asws_metrics"),
+				zap.String("version", Version),
+				zap.String("port", *metricsPort),
+				zap.String("ip", *ip),
+			)
+
+			err := http.ListenAndServe(*ip+":"+*metricsPort, nil)
+			if err != nil {
+				logger.Fatal("Error Starting ASWS Metrics Server", zap.Error(err))
+				os.Exit(1)
+			}
 		}()
 	}
 
+	logger.Info("Starting ASWS Server",
+		zap.String("type", "start_asws"),
+		zap.String("version", Version),
+		zap.String("port", *port),
+		zap.String("ip", *ip),
+	)
+
 	// Gin web server
-	err := r.Run(":" + port)
+	err := r.Run(*ip + ":" + *port)
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/txn2/asws
 
 go 1.15
+
+require (
+	github.com/gin-contrib/zap v0.0.1 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/prometheus/client_golang v1.8.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+)
